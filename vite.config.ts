bf406ea807291648c9e999cd9fc268631e671062
badriev <am.badriev@gmail.com>
2024-10-26 23:59:15 +0400

    fixed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import react from "@vitejs/plugin-react";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/au-cv",
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "PROD" ? "/au-cv" : "/",
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
     },
-  },
+  };
 });
