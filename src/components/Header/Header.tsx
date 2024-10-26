34f41d1034fe97c780ced1e1d27abbf723b4f383
badriev <am.badriev@gmail.com>
2024-10-27 00:25:49 +0400

    minor

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import FlexboxRow from "../FlexboxRow";
 import classes from "./Header.module.css";
+import Emoji from "/Emoji.png";
 
 const Header = () => {
   return (
     <FlexboxRow className={classes.headerContainer}>
-      <FlexboxRow style={{gap: '16px'}}>
-        <img src="/Emoji.png" alt="img" className={classes.headerEmojiImg}/>
+      <FlexboxRow style={{ gap: "16px" }}>
+        <img src={Emoji} alt="img" className={classes.headerEmojiImg} />
         <p>Юлтыев Айнур</p>
       </FlexboxRow>
       <p>UI UX дизайнер</p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home/Home";
 
 const router = createBrowserRouter([
-  {path: "/", element: <Home />},
-  { path: "*", element: <Navigate to="/" /> },
+  { path: "*", element: <Home /> },
+  // { path: "*", element: <Navigate to="/" /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
